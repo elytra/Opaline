a4cc819c3a3b6b796fdb1e4435f62f891f8e9a98
Meredith Espinosa <goclonefilms@gmail.com>
2017-10-26 00:20:10 -0700

    Working on implementing tile entity for distiller

--- src/main/java/com/elytradev/opaline/tile/TileEntityDistiller.java
+++ src/main/java/com/elytradev/opaline/tile/TileEntityDistiller.java
@@ -1,6 +1,61 @@
 package com.elytradev.opaline.tile;
 
+import com.elytradev.opaline.util.FluidAccess;
+import com.elytradev.opaline.util.ObservableFluidStorage;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.Constants;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidTank;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class TileEntityDistiller extends TileEntity {
+import javax.annotation.Nullable;
+
+public class TileEntityDistiller extends TileEntity implements ITickable {
+
+    private ObservableFluidStorage tank;
+    private int processTime;
+    private int lastTankAmount;
+    private int lastProcessTime;
+
+    public TileEntityDistiller() {
+        this.tank = new ObservableFluidStorage(300);
+        tank.listen(this::markDirty);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public int getTankScaled(int i){
+        return this.tank.getFluidAmount()*i/this.tank.getCapacity();
+    }
+
+
+
+    public void update() {
+
+    }
+
+    @Override
+    public boolean hasCapability(Capability<?> capability, @Nullable EnumFacing facing) {
+        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) {
+            return true;
+        } else {
+            return super.hasCapability(capability, facing);
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T getCapability(Capability<T> capability, @Nullable EnumFacing facing) {
+        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) {
+            return (T) FluidAccess.extractOnly(tank);
+        } else {
+            return super.getCapability(capability, facing);
+        }
+    }
 }
